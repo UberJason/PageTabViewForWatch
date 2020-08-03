4eb9d54f7e4ea6255a2d9d58aec020e79096c108
Jason Ji <uberjason@gmail.com>
2020-08-03 09:37:13 -0400

    Initial Commit

---
+++ PageTabViewForWatch WatchKit Extension/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  PageTabViewForWatch WatchKit Extension
+//
+//  Created by Jason Ji on 8/3/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PageTabViewForWatch WatchKit Extension/PageTabViewForWatchApp.swift
@@ -0,0 +1,19 @@
+//
+//  PageTabViewForWatchApp.swift
+//  PageTabViewForWatch WatchKit Extension
+//
+//  Created by Jason Ji on 8/3/20.
+//
+
+import SwiftUI
+
+@main
+struct PageTabViewForWatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
